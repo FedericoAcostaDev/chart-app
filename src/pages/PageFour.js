e0696728c5ca10655c0ecbc30842eac6bf08c8ea
Federico <fedeacos045@gmail.com>
2021-10-28 19:38:44 -0300

    first commit

---
+++ src/pages/PageFour.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageFour = () => {
+  return <h1>page four</h1>;
+};
+
+export default PageFour;
---
+++ src/pages/PageOne.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class FetchRandomUser extends React.Component {
+  state = {
+    loading: true,
+    image: null,
+  };
+
+  async componentDidMount() {
+    const url = "https://dev-cognitive-dashboard-server.herokuapp.com/techtest";
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.log(data.filter((data) => data.type === 0)); // this filter is fine
+    //console.log(data.map((data) => data.sector));
+
+    this.setState({
+      image: data.filter((data) => data.type === 0),
+      loading: false,
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.image) {
+      return <div>didn't get an image</div>;
+    }
+    //console.log(this.state.image[0]);
+    //console.log(this.state);
+
+    let eachImage = "";
+    for (let i = 0; i < this.state.image.length; i++) {
+      let img_src = this.state.image[i].url_original;
+      //console.log(img_src);
+      eachImage += `${(
+        <img alt="" href={window.webkitURL.createObjectURL(img_src)} />
+      )}`;
+    }
+
+    //console.log(eachImage);
+    return eachImage;
+  }
+}
