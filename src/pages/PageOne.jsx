133ca6ee106c33429d27a6ed433b27b68aa2ebc2
Federico <fedeacos045@gmail.com>
2021-10-29 12:22:46 -0300

    fixed GET images

--- src/pages/PageOne.jsx
+++ src/pages/PageOne.jsx
@@ -14,7 +14,7 @@ export default class FetchRandomUser extends React.Component {
     //console.log(data.map((data) => data.sector));
 
     this.setState({
-      image: data.filter((data) => data.type === 0),
+      image: data.filter((elem) => elem.type === 0).splice(0, 5),
       loading: false,
     });
   }
@@ -27,17 +27,29 @@ export default class FetchRandomUser extends React.Component {
     if (!this.state.image) {
       return <div>didn't get an image</div>;
     }
+
+    return (
+      <div>
+        {this.state.image.map((image) => (
+          <img alt="alt" src={image.url_original} key={image.url_original} />
+        ))}
+      </div>
+    );
+
+    //DELETE ALL DOWN BELOw
     //console.log(this.state.image[0]);
     //console.log(this.state);
 
-    let eachImage = "";
-    for (let i = 0; i < this.state.image.length; i++) {
-      let img_src = this.state.image[i].url_original;
-      console.log(img_src);
-      eachImage += `${(<img alt="" href={img_src} />)}`;
-    }
+    // let eachImage = "";
+    // for (let i = 0; i < this.state.image.length; i++) {
+    //   let img_src = this.state.image[i].url_original;
+    //   //console.log(img_src);
+    //   eachImage += `${(
+    //     <img alt="" href={window.webkitURL.createObjectURL(img_src)} />
+    //   )}`;
+    // }
 
-    //console.log(eachImage);
-    return eachImage;
+    // //console.log(eachImage);
+    // return eachImage;
   }
 }
