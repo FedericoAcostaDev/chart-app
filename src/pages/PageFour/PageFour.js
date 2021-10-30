4a86700f0856dffa46a59be72a401d9db6f69461
Federico <fedeacos045@gmail.com>
2021-10-30 11:57:45 -0300

    added charts

--- src/pages/PageFour/PageFour.js
+++ src/pages/PageFour/PageFour.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Model1 from "../../components/dashboard/chart1";
+import Model2 from "../../components/dashboard/chart2";
+import Model3 from "../../components/dashboard/chart3";
+import ItemHeight from "../../components/dashboard/height";
+import PerFrame from "../../components/dashboard/PerFrame";
 
 const PageFour = () => {
-  return <h1>page four</h1>;
+  return (
+    <div>
+      <h1>page four</h1>
+      <ItemHeight />
+      <Model1 />
+      <Model2 />
+      <Model3 />
+      <PerFrame />
+    </div>
+  );
 };
 
 export default PageFour;
