de92c7560b8b3a39c6095495e1fb1cc858c17e11
Federico <fedeacos045@gmail.com>
2021-10-29 22:12:57 -0300

    added all files category

--- src/pages/PageThree/PageThree.js
+++ src/pages/PageThree/PageThree.js
@@ -1,7 +1,18 @@
 import React from "react";
-
-const PageThree = () => {
-    return <h1>Page Three</h1>;
+import { Container } from "@material-ui/core";
+import GaleryVideos from "../../components/Galery/GaleryVideos";
+import Galery from "../../components/Galery/Galery";
+const PageThree = (props) => {
+  return (
+    <Container
+      style={{
+        marginTop: "5vh",
+      }}
+    >
+      <GaleryVideos />
+      <Galery />
+    </Container>
+  );
 };
 
 export default PageThree;
