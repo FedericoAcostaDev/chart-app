3068cc1151107c568e5e88465a8fbae387974c6c
Federico <fedeacos045@gmail.com>
2021-11-01 11:04:02 -0300

    trying get props

--- src/pages/PageFour/PageFour.js
+++ src/pages/PageFour/PageFour.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { Box, Container, Grid } from "@material-ui/core";
-import Model1 from "../../components/dashboard/chart1";
-import Model2 from "../../components/dashboard/chart2";
-import Model3 from "../../components/dashboard/chart3";
-import ItemHeight from "../../components/dashboard/height";
-import PerFrame from "../../components/dashboard/PerFrame";
-import ItemWidth from "../../components/dashboard/Width";
+import Dashboard from "../../components/dashboard/Dashboard";
 
 const PageFour = () => {
   return (
@@ -18,41 +13,8 @@ const PageFour = () => {
         <title>File's Metrics</title>
       </h1>
       <br />
-      <Box
-        sx={{
-          backgroundColor: "background.default",
-          minHeight: "100%",
-          py: 3,
-        }}
-      >
-        <Container maxWidth={false}>
-          <Grid container spacing={3}>
-            <Grid item lg={3} sm={6} xl={3} xs={12}>
-              <ItemHeight />
-            </Grid>
-            <Grid item lg={3} sm={6} xl={3} xs={12}>
-              <ItemWidth />
-            </Grid>
-            <Grid item lg={3} sm={6} xl={3} xs={12}>
-              <ItemHeight />
-            </Grid>
-            <Grid item lg={3} sm={6} xl={3} xs={12}>
-              <ItemHeight />
-            </Grid>
-            <Grid item lg={6} md={6} xl={8} xs={12}>
-              <PerFrame />
-            </Grid>
-            <Grid item lg={6} md={6} xl={4} xs={12}>
-              <Model1 sx={{ height: "100%" }} />
-            </Grid>
-            <Grid item lg={6} md={6} xl={4} xs={12}>
-              <Model2 sx={{ height: "100%" }} />
-            </Grid>
-            <Grid item lg={6} md={6} xl={4} xs={12}>
-              <Model3 />
-            </Grid>
-          </Grid>
-        </Container>
+      <Box>
+        <Dashboard />
       </Box>
     </div>
   );
